f91a164cdd6b56e55b48e372ca90cbca35345d49
sheriffkule <zeljko225@yahoo.com>
2024-01-22 13:41:07 +0100

    working with the "switch-case" statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -33,40 +33,83 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth,
   };
-  if (ev === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = "MONSTER";
-  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+switch (ev) {
+  case LOG_EVENT_PLAYER_ATTACK:
+    logEntry.target = 'MONSTER';
+    break;
+  case LOG_EVENT_PLAYER_STRONG_ATTACK:
     logEntry = {
       event: ev,
       value: val,
-      target: "MONSTER",
+      target: 'MONSTER',
       finalMonsterHealth: monsterHealth,
       finalPlayerHealth: playerHealth,
     };
-  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+    break;
+  case LOG_EVENT_MONSTER_ATTACK:
     logEntry = {
       event: ev,
       value: val,
-      target: "PLAYER",
+      target: 'PLAYER',
       finalMonsterHealth: monsterHealth,
       finalPlayerHealth: playerHealth,
     };
-  } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+    break;
+  case LOG_EVENT_PLAYER_HEAL:
     logEntry = {
       event: ev,
       value: val,
-      target: "PLAYER",
+      target: 'PLAYER',
       finalMonsterHealth: monsterHealth,
       finalPlayerHealth: playerHealth,
     };
-  } else if (ev === LOG_EVENT_GAME_OVER) {
+    break;
+  case LOG_EVENT_GAME_OVER:
     logEntry = {
       event: ev,
       value: val,
       finalMonsterHealth: monsterHealth,
       finalPlayerHealth: playerHealth,
     };
-  }
+    break;
+  default:
+    logEntry = {};
+}
+
+  // if (ev === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = "MONSTER";
+  // } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "MONSTER",
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth,
+  //   };
+  // } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "PLAYER",
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth,
+  //   };
+  // } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "PLAYER",
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth,
+  //   };
+  // } else if (ev === LOG_EVENT_GAME_OVER) {
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth,
+  //   };
+  // }
   battleLog.push(logEntry);
 }
 
